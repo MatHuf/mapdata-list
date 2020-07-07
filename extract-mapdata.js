3df81ea6dda460b85c7076c95f38117108aef641
MatHuf <53545249+MatHuf@users.noreply.github.com>
2020-07-07 16:05:07 -0400

    Create and save html file

--- extract-mapdata.js
+++ extract-mapdata.js
@@ -8,6 +8,8 @@ const countries = require("./countries.js");
 eval(fs.readFileSync("./Example/mapdata.js").toString());
 
 const locationsWithCountries = [];
+const uniqueCountries = [];
+let locationsRemaining = Object.keys(simplemaps_worldmap_mapdata.locations).length;
 
 for (let key in simplemaps_worldmap_mapdata.locations) {
   let location = simplemaps_worldmap_mapdata.locations[key];
@@ -17,19 +19,58 @@ for (let key in simplemaps_worldmap_mapdata.locations) {
       return;
     }
     let country = countries.countryListAlpha2[code.toUpperCase()];
+    if (!uniqueCountries.includes(country)) {
+      uniqueCountries.push(country);
+    }
     locationsWithCountries.push({ ...location, country: country });
-    console.log(locationsWithCountries);
+    locationsRemaining--;
+    if (!locationsRemaining) {
+      uniqueCountries.sort((a, b) => a.localeCompare(b));
+      const html = createHtml();
+      fs.writeFile('./Output/locationsByCountry.html', html, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+    }
   };
   grid.getCode(Number.parseFloat(location.lat), Number.parseFloat(location.lng), setCode);
 }
 
-console.log(countries);
+// https://stackoverflow.com/questions/21617468/node-js-generate-html
+const createHtml = () => {
+  const header = '';
+  return `<!DOCTYPE html><html><head>${header}</head><body>${createCountryLinks()}${createBody()}</body></html>`;
+};
 
+const createBody = () => {
+  let countryItems = '';
+  for (let country of uniqueCountries) {
+    const locationsInCountry = locationsWithCountries.filter(l => l.country === country);
+    countryItems = countryItems + createCountryItem(locationsInCountry, country);
+  }
+  return countryItems;
+};
 
-// group by country
-// sort?
-// create href
-// write html doc
-// html structure
-// list of counties that link to list of events for that country
-// list of events by country
+const createCountryItem = (locations, country) => {
+  let list = '';
+  for (let location of locations) {
+    list = list + createLocationItem(location);
+  }
+  return `<div><h2><a name='${country}'>${country}</a></h2><ul>${list}</ul></div>`;
+};
+
+const createLocationItem = (location) => {
+  return `<li><div><a href='${location.url}' target='_blank'>${location.name}</a><p>${location.description}</p></div></li>`;
+};
+
+const createCountryLinks = () => {
+  let links = '';
+  for (let country of uniqueCountries) {
+    links = links + createCountryLink(country);
+  }
+  return `<nav><ul>${links}</ul></nav>`;
+};
+
+const createCountryLink = (country) => {
+  return `<li><a href='#${country}'>${country}</a></li>`;
+};
